3a98ba325131994e783d91a89b35c564e3e6bee0
Vaibhav Mujumdar <123071687+vaibhavmujumdar08@users.noreply.github.com>
2024-06-21 12:30:45 +0530

    Initial commit

---
+++ frontend-react/src/Data/topMeels.js
@@ -0,0 +1,31 @@
+export const topMeels=[
+    {
+    image:"https://mylifecookbook.com/wp-content/uploads/2020/01/burrito-bowl-f4.jpg",
+    title:"Ketogenic Diet"
+},
+{
+    image:"https://images.ctfassets.net/9kmxuqcmuyyb/6CcDjTGVZ9FqYB7wL0bqlG/bc6cfe8e449d5eba2da3e1ef6e58fe7e/falafel-bowl.webp",
+    title:"Mediterranean Diet"
+},
+{
+    image:"https://res.cloudinary.com/rainforest-cruises/images/c_fill,g_auto/f_auto,q_auto/w_1120,h_732,c_fill,g_auto/v1661347416/india-food-idli/india-food-idli-1120x732.jpg",
+    title:"Idli"
+},
+{
+    image:"https://cdn.loveandlemons.com/wp-content/uploads/2021/11/arugula-salad-1.jpg",
+    title:"Salads"
+},
+{
+    image:"https://cookieandkate.com/images/2015/11/vegetarian-chili-recipe-6-768x1154.jpg",
+    title:"Vegetarian Diet"
+},
+{
+    image:"https://www.eatingwell.com/thmb/mrTdd1XM_ywh1ZJcopmUDTC380U=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/peach-crisp-127fa354bfcd4e0b92d6c32791edd283.jpg",
+    title:"Deserts"
+},
+{   
+    image:"https://i.pinimg.com/564x/e4/6f/a7/e46fa71bb022ce9caf6a900b5077adc8.jpg",
+    title:"Indian Thalis"
+
+}
+]
